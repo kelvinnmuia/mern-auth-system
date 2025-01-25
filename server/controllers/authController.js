bcb2dc0c4d3ac8a4788b93857c8a5712bc2a66be
kelvinnmuia <kelvinnmuia@gmail.com>
2025-01-25 16:23:36 +0300

    created a user registration auth controller

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -21,6 +21,18 @@ export const register = async (req, res)=>{
         const user = new userModel({name, email, password: 
             hashedPassword});
             await user.save();
+
+            const token = jwt.sign({id: user._id}, process.env.JWT_SECRET,
+                { expiresIn: '7d'});
+
+            res.cookie('token', token, {
+                httpOnly: true,
+                secure: process.env.NODE_ENV === 'production',
+                sameSite: process.env.NODE_ENV === 'production' ? 
+                'none' : 'strict',
+                maxAge: 7 * 24 * 60 * 60 * 1000
+            })
+
     } catch (error) {
         res.json({success: false, message: error.message})
     }
