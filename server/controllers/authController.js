001e0ce7daa23189e6caa79fced65c39f0811fd6
kelvinnmuia <kelvinnmuia@gmail.com>
2025-01-27 12:31:19 +0300

    created an account verification controller function

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -105,3 +105,32 @@ export const logout = async (req, res)=>{
         return res.json({ success: false, message: error.message });
     }
 }
+
+export const sendVerifyOtp = async (req, res)=>{
+    try {
+        const {userId} = req.body;
+
+        const user = await userModel.findById(userId);
+
+        if(user.isAccountVerified){
+            return res.json({ success: false, message: "Account Already verified"})
+        }
+
+        const otp = String(Math.floor(100000 + Math.random() * 900000));
+
+        user.verifyOtp = otp;
+        user.verifyOtpExpireAt = Date.now() + 24 * 60 * 60 * 1000;
+
+        await user.save();
+
+        const mailOptions = {
+            from: process.env.SENDER_EMAIL,
+            to: user.email,
+            subject: 'Account Verification OTP',
+            text: `Your OTP is ${otp}. Verify your account using this OTP.`
+        }
+
+    } catch (error) {
+
+    }
+}
