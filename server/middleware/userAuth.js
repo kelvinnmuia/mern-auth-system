dc833dc2e06d2f6b1ae4e3e644721d0f0addbce6
kelvinnmuia <kelvinnmuia@gmail.com>
2025-01-28 06:26:47 +0300

    added docs for the userAuth middleware

--- server/middleware/userAuth.js
+++ server/middleware/userAuth.js
@@ -1,5 +1,16 @@
 import jwt from 'jsonwebtoken';
 
+/**
+ * A middleware function that verifies if the user is authenticated using a JWT token in the 'token' cookie.
+ * 
+ * If the token is valid, it adds the user's ID to the request body and calls the next middleware function.
+ * If the token is not valid, it sends a JSON response with a success property set to false and a message indicating that the user needs to login again.
+ * 
+ * @param {Object} req - The request object.
+ * @param {Object} res - The response object.
+ * @param {Function} next - The next middleware function in the stack.
+ * @returns {Promise<void>} - A promise that resolves when the middleware function is finished.
+ */
 const userAuth = async (req, res, next)=>{
     const {token} = req.cookies;
 
