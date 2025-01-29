ae13c2e991d2b14fb167cb3905155affe251812d
kelvinnmuia <kelvinnmuia@gmail.com>
2025-01-29 06:01:06 +0300

    added a bg-gradient for the login form

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
+import auth from '../assets/auth.png'
 
 function Login() {
+
+  const [state, setState] = useState('Sign Up')
   return (
-    <div>
-        <h1>Login page</h1>
+    <div className='flex items-center justify-center min-h-screen
+    px-6 sm:px-0 bg-gradient-to-br from-gray-250 to-cyan-100'>
+      <img src={auth} alt="" className="absolute left-5 sm:left-20
+      top-5 2-28 sm:w-32 cursor-pointer" />
+      <div>
+        <h2>{state === 'Sign Up' ? 'Create Account' : 'Login'}</h2>
+        <p>{state === 'Sign Up' ? 'Create your account' : 'Login to your account!'}</p>
+        <form>
+          <img src="" alt="" />
+          <input type="text" placeholder="Full Name" required />
+        </form>
+      </div>
     </div>
   )
 }
