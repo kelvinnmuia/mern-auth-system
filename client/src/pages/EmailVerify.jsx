7f528c13d4b06014424f9cf83ee3b28c59108568
kelvinnmuia <kelvinnmuia@gmail.com>
2025-01-30 12:25:54 +0300

    added a form for the email verify page

--- client/src/pages/EmailVerify.jsx
+++ client/src/pages/EmailVerify.jsx
@@ -7,8 +7,10 @@ function EmailVerify() {
     <div className='flex items-center justify-center min-h-screen 
     bg-gradient-to-br from-gray-200 to-cyan-100'>
       <img onClick={()=>navigate('/')} src={auth} alt="" className="absolute left-5 sm:left-20
-            top-5 2-28 sm:w-32 cursor-pointer" />
+            top-5 sm:w-32 cursor-pointer" />
+            <form className='bg-slate-900 p-8 rounded-lg shadow-lg w-96 text-sm'>
 
+            </form>
     </div>
   )
 }
