c80890728d19380129e2fc851502a9444d110e36
kelvinnmuia <kelvinnmuia@gmail.com>
2025-01-30 15:28:55 +0300

    updated the onsubmit function to fetch user data from the verify account api

--- client/src/pages/EmailVerify.jsx
+++ client/src/pages/EmailVerify.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import auth from '../assets/auth.png'
 import { useNavigate } from 'react-router-dom';
+import { AppContent } from '../context/AppContext';
+import axios from 'axios';
+import { toast } from 'react-toastify';
 
 function EmailVerify() {
+
+  axios.defaults.withCredentials = true;
+  const {backendUrl, isLoggedin, userData, getUserData} = useContext(AppContent)
+
+  const navigate = useNavigate()
 
   const inputRefs = React.useRef([])
 
@@ -34,9 +42,19 @@ function EmailVerify() {
       const otpArray = inputRefs.current.map(e => e.value)
       const otp = otpArray.join('')
 
-      const {data} = await axios.post()
-    } catch (error) {
+      const {data} = await axios.post(backendUrl + '/api/auth/verify-account', 
+        {otp}
+      )
 
+      if(data.success){
+        toast.success(data.message)
+        getUserData()
+        navigate('/')
+      }else{
+        toast.error(data.message)
+      }
+    } catch (error) {
+      toast.error(error.message)
     }
   }
 
@@ -45,7 +63,7 @@ function EmailVerify() {
     bg-gradient-to-br from-gray-200 to-cyan-100'>
       <img onClick={()=>navigate('/')} src={auth} alt="" className="absolute left-5 sm:left-20
             top-5 sm:w-32 cursor-pointer" />
-            <form className='bg-slate-800 p-8 rounded-lg shadow-lg w-96 text-sm'>
+            <form onSubmit={onSubmitHandler} className='bg-slate-800 p-8 rounded-lg shadow-lg w-96 text-sm'>
               <h1 className="text-white text-2xl font-semibold text-center mb-4">Email Verify OTP</h1>
               <p className="text-center mb-6 text-cyan-100">Enter the 6-digit code sent to your email address.</p>
               <div className="flex justify-between mb-8" onPaste={handlePaste}>
