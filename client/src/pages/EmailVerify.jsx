b385015f8682ff15222d0f75295d920536c69850
kelvinnmuia <kelvinnmuia@gmail.com>
2025-01-30 12:43:35 +0300

    changed the font color of the p tag to cyan

--- client/src/pages/EmailVerify.jsx
+++ client/src/pages/EmailVerify.jsx
@@ -8,9 +8,9 @@ function EmailVerify() {
     bg-gradient-to-br from-gray-200 to-cyan-100'>
       <img onClick={()=>navigate('/')} src={auth} alt="" className="absolute left-5 sm:left-20
             top-5 sm:w-32 cursor-pointer" />
-            <form className='bg-slate-900 p-8 rounded-lg shadow-lg w-96 text-sm'>
+            <form className='bg-slate-800 p-8 rounded-lg shadow-lg w-96 text-sm'>
               <h1 className="text-white text-2xl font-semibold text-center mb-4">Email Verify OTP</h1>
-              <p>Enter the 6-digit code sent to your email address.</p>
+              <p className="text-center mb-6 text-cyan-100">Enter the 6-digit code sent to your email address.</p>
             </form>
     </div>
   )
