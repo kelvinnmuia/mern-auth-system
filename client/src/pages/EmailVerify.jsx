b543b3512e52569c46ed4f66a3e015b97f37c556
kelvinnmuia <kelvinnmuia@gmail.com>
2025-01-30 12:16:28 +0300

    imported logo into the email verify page

--- client/src/pages/EmailVerify.jsx
+++ client/src/pages/EmailVerify.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import auth from '../assets/auth.png'
+import { useNavigate } from 'react-router-dom';
 
 function EmailVerify() {
   return (
-    <div className='flex items-center justify-center min-h-screen
-    px-6 sm:px-0 bg-gradient-to-br from-gray-200 to-cyan-100'>
+    <div className='flex items-center justify-center min-h-screen 
+    bg-gradient-to-br from-gray-200 to-cyan-100'>
+      <img onClick={()=>navigate('/')} src={auth} alt="" className="absolute left-5 sm:left-20
+            top-5 2-28 sm:w-32 cursor-pointer" />
 
     </div>
   )
