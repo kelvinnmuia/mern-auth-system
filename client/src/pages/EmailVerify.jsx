75db8830760de2623fba9db2a0f55f04cad6f771
kelvinnmuia <kelvinnmuia@gmail.com>
2025-01-30 13:37:30 +0300

    added 12px width and height for the otp input fields

--- client/src/pages/EmailVerify.jsx
+++ client/src/pages/EmailVerify.jsx
@@ -11,6 +11,13 @@ function EmailVerify() {
             <form className='bg-slate-800 p-8 rounded-lg shadow-lg w-96 text-sm'>
               <h1 className="text-white text-2xl font-semibold text-center mb-4">Email Verify OTP</h1>
               <p className="text-center mb-6 text-cyan-100">Enter the 6-digit code sent to your email address.</p>
+              <div className="flex justify-between mb-8">
+                {Array(6).fill(0).map((_, index)=>(
+                  <input type="text" maxLength='1' key={index} required
+                  className='w-12 h-12 bg-[#333A5C] text-white text-center text-xl rounded-md'/>
+                ))}
+              </div>
+              <button>Verify email</button>
             </form>
     </div>
   )
