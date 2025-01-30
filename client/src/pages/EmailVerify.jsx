e3341b883cb3816c759d813effe3bba0d0ec1ef5
kelvinnmuia <kelvinnmuia@gmail.com>
2025-01-30 12:11:05 +0300

    implemented a gradient backgorund on the email verify page

--- client/src/pages/EmailVerify.jsx
+++ client/src/pages/EmailVerify.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 function EmailVerify() {
   return (
-    <div>
-      <h1>Email Verify</h1>
+    <div className='flex items-center justify-center min-h-screen
+    px-6 sm:px-0 bg-gradient-to-br from-gray-200 to-cyan-100'>
+
     </div>
   )
 }
