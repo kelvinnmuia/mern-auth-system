7878e533eb022eac8154c804fa0faf73bf5380fd
kelvinnmuia <kelvinnmuia@gmail.com>
2025-02-03 12:13:01 +0300

    created comments and docs for the home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import Header from '../components/Header'
 
+/**
+ * The Home component renders the homepage of the website.
+ * It contains a Navbar and a Header and displays a background image.
+ */
 function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen 
